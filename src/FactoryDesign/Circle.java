079fa0da58b963017dc1e7a5861eb60dd41ea68e
arcesium-jena <jena@arcesium.com>
2025-01-27 18:02:18 +0530

    Initial code for factory pattern

---
+++ src/FactoryDesign/Circle.java
@@ -0,0 +1,9 @@
+package FactoryDesign;
+
+public class Circle implements Shape {
+
+    @Override
+    public String getName() {
+        return "Circle shape";
+    }
+}
---
+++ src/FactoryDesign/Rectangle.java
@@ -0,0 +1,8 @@
+package FactoryDesign;
+
+public class Rectangle implements Shape{
+    @Override
+    public String getName() {
+        return "Rectangle shape";
+    }
+}
